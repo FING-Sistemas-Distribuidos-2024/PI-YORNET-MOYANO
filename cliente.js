0782aaecc5a60eb66f2b078801db6d1bbb131e2b
Lucas <lucasmoyano11@outlook.it>
2024-06-05 21:41:35 -0300

    commit hecho para cambiar rama

--- cliente.js
+++ cliente.js
@@ -1,5 +1,6 @@
 async function setHighscoreOnServer(score) {
     let response = await fetch("http://127.0.0.1:3000")
-    let data = await response.json();
+    let data = await response;
+    console.log(data);
     return data;
 }
