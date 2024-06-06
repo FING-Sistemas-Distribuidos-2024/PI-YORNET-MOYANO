ee253112f08d547311ba7222ca4fa515c57f89bd
ElPelado619 <agustinyornet@gmail.com>
2024-06-06 16:46:53 -0300

    Agregado highscore global y front para 2048.

--- public/cliente.js
+++ public/cliente.js
@@ -1,4 +1,244 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const gridDisplay = document.querySelector(".grid")
+    const scoreDisplay = document.querySelector("#score")
+    const resultDisplay = document.querySelector("#result")
+    const width = 4
+    let squares = []
+    let score = 0
+
+    // create the playing board
+    function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement("div")
+            square.innerHTML = 0
+            gridDisplay.appendChild(square)
+            squares.push(square)
+        }
+        generate()
+        generate()
+    }
+    createBoard()
+
+    //generate a new number
+    function generate() {
+        const randomNumber = Math.floor(Math.random() * squares.length)
+        if (squares[randomNumber].innerHTML == 0) {
+            squares[randomNumber].innerHTML = 2
+            checkForGameOver()
+        } else generate()
+    }
+
+    function moveRight() {
+        for (let i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i + 1].innerHTML
+                let totalThree = squares[i + 2].innerHTML
+                let totalFour = squares[i + 3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                let filteredRow = row.filter(num => num)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                let newRow = zeros.concat(filteredRow)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i + 1].innerHTML = newRow[1]
+                squares[i + 2].innerHTML = newRow[2]
+                squares[i + 3].innerHTML = newRow[3]
+            }
+        }
+    }
+
+    function moveLeft() {
+        for (let i = 0; i < 16; i++) {
+            if (i % 4 === 0) {
+                let totalOne = squares[i].innerHTML
+                let totalTwo = squares[i + 1].innerHTML
+                let totalThree = squares[i + 2].innerHTML
+                let totalFour = squares[i + 3].innerHTML
+                let row = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+                let filteredRow = row.filter(num => num)
+                let missing = 4 - filteredRow.length
+                let zeros = Array(missing).fill(0)
+                let newRow = filteredRow.concat(zeros)
+
+                squares[i].innerHTML = newRow[0]
+                squares[i + 1].innerHTML = newRow[1]
+                squares[i + 2].innerHTML = newRow[2]
+                squares[i + 3].innerHTML = newRow[3]
+            }
+        }
+    }
+
+    function moveUp() {
+        for (let i = 0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i + width].innerHTML
+            let totalThree = squares[i + width * 2].innerHTML
+            let totalFour = squares[i + width * 3].innerHTML
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            let filteredColumn = column.filter(num => num)
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = filteredColumn.concat(zeros)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i + width].innerHTML = newColumn[1]
+            squares[i + width * 2].innerHTML = newColumn[2]
+            squares[i + width * 3].innerHTML = newColumn[3]
+        }
+    }
+
+    function moveDown() {
+        for (let i = 0; i < 4; i++) {
+            let totalOne = squares[i].innerHTML
+            let totalTwo = squares[i + width].innerHTML
+            let totalThree = squares[i + width * 2].innerHTML
+            let totalFour = squares[i + width * 3].innerHTML
+            let column = [parseInt(totalOne), parseInt(totalTwo), parseInt(totalThree), parseInt(totalFour)]
+
+            let filteredColumn = column.filter(num => num)
+            let missing = 4 - filteredColumn.length
+            let zeros = Array(missing).fill(0)
+            let newColumn = zeros.concat(filteredColumn)
+
+            squares[i].innerHTML = newColumn[0]
+            squares[i + width].innerHTML = newColumn[1]
+            squares[i + width * 2].innerHTML = newColumn[2]
+            squares[i + width * 3].innerHTML = newColumn[3]
+        }
+    }
+
+    function combineRow() {
+        for (let i = 0; i < 15; i++) {
+            if (squares[i].innerHTML === squares[i + 1].innerHTML) {
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i + 1].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i + 1].innerHTML = 0
+                score += combinedTotal
+                scoreDisplay.innerHTML = score
+            }
+        }
+        checkForWin()
+    }
+
+    function combineColumn() {
+        for (let i = 0; i < 12; i++) {
+            if (squares[i].innerHTML === squares[i + width].innerHTML) {
+                let combinedTotal = parseInt(squares[i].innerHTML) + parseInt(squares[i + width].innerHTML)
+                squares[i].innerHTML = combinedTotal
+                squares[i + width].innerHTML = 0
+                score += combinedTotal
+                scoreDisplay.innerHTML = score
+            }
+        }
+        checkForWin()
+    }
+
+    ///assign functions to keys
+    function control(e) {
+        if (e.key === "ArrowLeft") {
+            keyLeft()
+        } else if (e.key === "ArrowRight") {
+            keyRight()
+        } else if (e.key === "ArrowUp") {
+            keyUp()
+        } else if (e.key === "ArrowDown") {
+            keyDown()
+        }
+    }
+    document.addEventListener("keydown", control)
+
+    function keyLeft() {
+        moveLeft()
+        combineRow()
+        moveLeft()
+        generate()
+    }
+
+    function keyRight() {
+        moveRight()
+        combineRow()
+        moveRight()
+        generate()
+    }
+
+    function keyUp() {
+        moveUp()
+        combineColumn()
+        moveUp()
+        generate()
+    }
+
+    function keyDown() {
+        moveDown()
+        combineColumn()
+        moveDown()
+        generate()
+    }
+
+    //check for the number 2048 in the squares to win
+    function checkForWin() {
+        for (let i = 0; i < squares.length; i++) {
+            if (squares[i].innerHTML == 2048) {
+                resultDisplay.innerHTML = "You WIN!"
+                document.removeEventListener("keydown", control)
+                setHighscoreOnServer();
+                setTimeout(clear, 3000)
+            }
+        }
+    }
+
+    //check if there are no zeros on the board to lose
+    function checkForGameOver() {
+        let zeros = 0
+        for (let i = 0; i < squares.length; i++) {
+            if (squares[i].innerHTML == 0) {
+                zeros++
+            }
+        }
+        if (zeros === 0) {
+            resultDisplay.innerHTML = "You LOSE!"
+            document.removeEventListener("keydown", control)
+            setHighscoreOnServer();
+            setTimeout(clear, 3000)
+        }
+    }
+
+    function clear() {
+        clearInterval(myTimer)
+    }
+
+    //add colours
+    function addColours() {
+        for (let i = 0; i < squares.length; i++) {
+            if (squares[i].innerHTML == 0) squares[i].style.backgroundColor = "#afa192"
+            else if (squares[i].innerHTML == 2) squares[i].style.backgroundColor = "#eee4da"
+            else if (squares[i].innerHTML == 4) squares[i].style.backgroundColor = "#ede0c8"
+            else if (squares[i].innerHTML == 8) squares[i].style.backgroundColor = "#f2b179"
+            else if (squares[i].innerHTML == 16) squares[i].style.backgroundColor = "#ffcea4"
+            else if (squares[i].innerHTML == 32) squares[i].style.backgroundColor = "#e8c064"
+            else if (squares[i].innerHTML == 64) squares[i].style.backgroundColor = "#ffab6e"
+            else if (squares[i].innerHTML == 128) squares[i].style.backgroundColor = "#fd9982"
+            else if (squares[i].innerHTML == 256) squares[i].style.backgroundColor = "#ead79c"
+            else if (squares[i].innerHTML == 512) squares[i].style.backgroundColor = "#76daff"
+            else if (squares[i].innerHTML == 1024) squares[i].style.backgroundColor = "#beeaa5"
+            else if (squares[i].innerHTML == 2048) squares[i].style.backgroundColor = "#d7d4f0"
+        }
+    }
+    addColours()
+
+    let myTimer = setInterval(addColours, 50)
+})
+
 document.addEventListener('DOMContentLoaded', (event) => {
+    getHighscoreFromServer();
+});
+
+document.addEventListener('keydown', (event) => {
     getHighscoreFromServer();
 });
 
@@ -9,7 +249,7 @@ async function getHighscoreFromServer() {
             throw new Error('Network response was not ok');
         }
         let data = await response.json();
-        document.getElementById('current-highscore').textContent = `Current Highscore: ${data.highscore}`;
+        document.getElementById('current-highscore').textContent = `${data.highscore}`;
     } catch (error) {
         console.error('Error:', error);
         document.getElementById('current-highscore').textContent = 'Failed to load highscore';
@@ -17,12 +257,15 @@ async function getHighscoreFromServer() {
 }
 
 async function setHighscoreOnServer() {
-    const score = document.getElementById('score').value;
+    const score = parseInt(document.getElementById('score').innerHTML);
+    console.log(score);
 
     let response = await fetch("/highscore");
     let data = await response.json();
+    console.log(data.highscore);
+    console.log(score > data.highscore);
 
-    if (score > data.highscore){
+    if (score > data.highscore) {
         try {
             let response = await fetch("/highscore", {
                 method: 'POST',
@@ -31,14 +274,14 @@ async function setHighscoreOnServer() {
                 },
                 body: JSON.stringify({ highscore: score })
             });
-    
+
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-    
+
             let data = await response.json();
             console.log(data);
-            document.getElementById('current-highscore').textContent = `Current Highscore: ${data.highscore}`;
+            document.getElementById('current-highscore').textContent = `${data.highscore}`;
             return data;
         } catch (error) {
             console.error('Error:', error);
@@ -46,3 +289,5 @@ async function setHighscoreOnServer() {
     }
 
 }
+
+
