14851cea98b709094a7f18fa3625f710f8a6d75a
Lucas <lucasmoyano11@outlook.it>
2024-06-14 17:10:36 -0300

    the frontend now receives text

--- client/cliente.js
+++ client/cliente.js
@@ -1,6 +1,6 @@
 async function setHighscoreOnServer(score) {
     let response = await fetch("http://localhost:3000/highscore")
-    let data = await response;
+    let data = await response.text();
     console.log(data);
     return data;
 }
